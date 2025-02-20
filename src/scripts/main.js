8d5a7f9389d908dc1c166ff4e4063918a5e63fe9
Nataliia Kozyriatska <kozyriatskanataliia@gmail.com>
2025-02-20 20:37:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+async function filterPromise() {
+  try {
+    await Promise.resolve();
+
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  } catch (error) {
+    const rejectionError = new Error('Promise was rejected!');
+
+    Promise.reject(rejectionError);
+
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = 'Promise was rejected! ' + rejectionError.message;
+    document.body.appendChild(div);
+  }
+}
+
+document.querySelector('.logo').addEventListener('click', filterPromise);
